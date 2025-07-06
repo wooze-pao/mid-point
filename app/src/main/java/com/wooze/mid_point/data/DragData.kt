0e2920e5e38f68be002fecc7670cee2965378933
Woozepao <xiaobbs@outlook.com>
2025-07-06 00:10:10 -0500

    修改定义DragData数据类，移除没必要的clipData

--- app/src/main/java/com/wooze/mid_point/data/DragData.kt
+++ app/src/main/java/com/wooze/mid_point/data/DragData.kt
@@ -3,4 +3,4 @@ package com.wooze.mid_point.data
 import android.content.ClipData
 import android.net.Uri
 
-data class DragData(val uri: Uri, val clipData: ClipData, val mimetype: String?)
+data class DragData(val uri: Uri, val mimetype: String?)
