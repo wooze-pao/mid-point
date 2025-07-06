041d252701e8b65fab8766d7a4223d97118f27bb
Woozepao <xiaobbs@outlook.com>
2025-07-06 00:14:17 -0500

    格式

--- app/src/main/java/com/wooze/mid_point/ui/theme/Theme.kt
+++ app/src/main/java/com/wooze/mid_point/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.wooze.mid_point.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
