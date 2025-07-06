304dbaf9ad17ba09244357acb78bd72de52f4c0f
Woozepao <xiaobbs@outlook.com>
2025-07-06 00:13:40 -0500

    添加分享activty 完成规划中的3启动方式

---
+++ app/src/main/java/com/wooze/mid_point/shareActivity.kt
@@ -0,0 +1,45 @@
+package com.wooze.mid_point
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import com.wooze.mid_point.data.DragData
+import com.wooze.mid_point.objects.FloatWindowAction
+import com.wooze.mid_point.state.UiState
+
+class ShareActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d("ShareActivity", "onCreate called")
+        moveTaskToBack(true)
+        handleShareIntent(intent)
+        finish()
+    }
+
+
+    @SuppressLint("UnsafeIntentLaunch")
+    private fun handleShareIntent(intent: Intent?) {
+        var uri : Uri?
+        if (intent?.action == Intent.ACTION_SEND) {
+            uri = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                intent.getParcelableExtra(Intent.EXTRA_STREAM, Uri::class.java)
+            } else {
+                @Suppress("DEPRECATION")
+                intent.getParcelableExtra(Intent.EXTRA_STREAM) as? Uri
+            }
+            uri?.let { uri ->
+                grantUriPermission(packageName,uri,Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                val mimetype = contentResolver.getType(uri)
+                val dragData = DragData(uri,mimetype)
+                UiState.dragDataList.add(dragData)
+                if (!UiState.isShowing) {
+                    FloatWindowAction.openFloatActivity(this)
+                }
+            }
+        }
+    }
+}
