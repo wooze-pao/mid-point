11c5e176cf607a1876c28e35d3857403da4e6643
Woozepao <xiaobbs@outlook.com>
2025-07-06 00:09:12 -0500

    添加新导入

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation("com.github.bumptech.glide:compose:1.0.0-beta01")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
