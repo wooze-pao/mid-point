40e3d951b62b34e01eb737babba668e1ad83a15a
Woozepao <xiaobbs@outlook.com>
2025-07-10 09:48:39 -0500

    添加新图标

--- app/src/main/java/com/wooze/mid_point/TypeCategory.kt
+++ app/src/main/java/com/wooze/mid_point/TypeCategory.kt
@@ -6,8 +6,16 @@ fun typeCategory(data: DragData): Any {
     // TODO
     if (data.mimetype != null) {
         return when {
-            data.mimetype.startsWith("image/") -> {
+            data.mimetype.startsWith("image/") || data.mimetype.startsWith("video/") -> { // 图片和视频
                 data.uri
+            }
+
+            data.mimetype.startsWith("audio/") -> { // 音频
+                R.drawable.icon_color_audio
+            }
+
+            data.mimetype.startsWith("text/") -> { // 文本
+                R.drawable.icon_color__doc
             }
 
             else -> {
@@ -16,5 +24,5 @@ fun typeCategory(data: DragData): Any {
 
         }
     }
-    return 1
+    return R.drawable.icon_color_skt
 }
