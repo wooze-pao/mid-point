b7477816466cd64e40322542a686ae26112f2c4c
Woozepao <xiaobbs@outlook.com>
2025-07-12 22:31:40 -0500

    版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.wooze.mid_point"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "demo - 1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
