0042aa88b607e3d6a51d227048679d406f311a5d
Woozepao <xiaobbs@outlook.com>
2025-07-13 21:01:19 -0500

    viewmodel添加

--- app/src/main/java/com/wooze/mid_point/MainActivity.kt
+++ app/src/main/java/com/wooze/mid_point/MainActivity.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.wooze.mid_point.objects.FloatWindowAction.closeFloatActivity
 import com.wooze.mid_point.objects.FloatWindowAction.openFloatActivity
 import com.wooze.mid_point.ui.homeScreenUi.HomeScreen
@@ -14,10 +15,10 @@ import com.wooze.mid_point.viewModel.MainViewModel
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val viewModel: MainViewModel = MainViewModel()
         Log.d("TaskTest", "MainActivity taskId = $taskId")
         enableEdgeToEdge()
         setContent {
+            val viewModel: MainViewModel = viewModel()
             MidPointTheme {
                 HomeScreen({ openFloatActivity(this) }, { closeFloatActivity() }, viewModel)
             }
