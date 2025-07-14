3461fd21acc2706152f09d7aa42d00e995278379
Woozepao <xiaobbs@outlook.com>
2025-07-13 20:56:46 -0500

    添加唯一id

--- app/src/main/java/com/wooze/mid_point/data/DragData.kt
+++ app/src/main/java/com/wooze/mid_point/data/DragData.kt
@@ -1,5 +1,6 @@
 package com.wooze.mid_point.data
 
 import android.net.Uri
+import java.util.UUID
 
-data class DragData(val uri: Uri, val mimetype: String?)
+data class DragData(val uri: Uri, val mimetype: String?,val id: String = UUID.randomUUID().toString()) // 新增唯一id
