bdc16dc04b56d281aa0350e2d4440a1f5c483125
Woozepao <xiaobbs@outlook.com>
2025-08-04 19:23:23 -0500

    if 改 when

--- app/src/main/java/com/wooze/mid_point/shareActivity.kt
+++ app/src/main/java/com/wooze/mid_point/shareActivity.kt
@@ -21,35 +21,42 @@ class ShareActivity : ComponentActivity() {
     private fun handleShareIntent(intent: Intent) {
         var uri: Uri?
         var text: String?
-        if (intent.action == Intent.ACTION_SEND) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                uri = intent.getParcelableExtra(Intent.EXTRA_STREAM, Uri::class.java)
-            } else {
-                @Suppress("DEPRECATION")
-                uri = intent.getParcelableExtra(Intent.EXTRA_STREAM) as? Uri
-            }
-            text = intent.getStringExtra(Intent.EXTRA_TEXT)
-            text?.let { text ->
-                DataTools.extractAndSave(text, this)
-            }
-            uri?.let { uri ->
-                DataTools.extractAndSave(uri, this)
-            }
-        } else if (intent.action == Intent.ACTION_SEND_MULTIPLE) {
-            val uriList: List<Uri>? = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM, Uri::class.java)
-            } else {
-                @Suppress("DEPRECATION")
-                intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM)
+        when (intent.action) {
+            Intent.ACTION_SEND -> { // 单个分享
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    uri = intent.getParcelableExtra(Intent.EXTRA_STREAM, Uri::class.java)
+                } else {
+                    @Suppress("DEPRECATION")
+                    uri = intent.getParcelableExtra(Intent.EXTRA_STREAM) as? Uri
+                }
+                text = intent.getStringExtra(Intent.EXTRA_TEXT)
+                text?.let { text ->
+                    DataTools.extractAndSave(text, this)
+                }
+                uri?.let { uri ->
+                    DataTools.extractAndSave(uri, this)
+                }
             }
 
-            uriList?.let { uri ->
-                DataTools.extractAndSave(uri, this)
+            Intent.ACTION_SEND_MULTIPLE -> { // 多个分享
+                val uriList: List<Uri>? =
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                        intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM, Uri::class.java)
+                    } else {
+                        @Suppress("DEPRECATION")
+                        intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM)
+                    }
+
+                uriList?.let { uri ->
+                    DataTools.extractAndSave(uri, this)
+                }
             }
-        } else if (intent.action == Intent.ACTION_VIEW) {
-            val uri = intent.data
-            uri?.let { uri ->
-                DataTools.extractAndSave(uri, this)
+
+            Intent.ACTION_VIEW -> { // 打开方式分享
+                val uri = intent.data
+                uri?.let { uri ->
+                    DataTools.extractAndSave(uri, this)
+                }
             }
         }
     }
