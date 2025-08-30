ed501996b9f5530b3a020bf4680b902633656ad3
wooze-pao <xiaobbs@outlook.com>
2025-08-30 16:11:44 -0500

    添加设置页面

--- app/src/main/java/com/wooze/mid_point/ui/SettingPage.kt
+++ app/src/main/java/com/wooze/mid_point/ui/SettingPage.kt
@@ -15,6 +15,137 @@ fun SettingPage() {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text("hello world")
+        CategoryItems(
+            listOf(
+                SettingItemData.SwitchItem(
+                    "自动收起",
+                    "关闭磁贴自动收起快捷设置",
+                    { viewModel.changeData(it) },
+                    viewModel.isAutoCollapse.value
+                ),
+                SettingItemData.ClickItem(
+                    "添加磁贴",
+                    "唤起添加磁贴对话框",
+                    { viewModel.requestAddTile(context) }
+                )
+            ),
+            "磁贴"
+        )
+    }
+}
+
+@Composable
+fun CategoryItems(
+    list: List<SettingItemData>, topLabel: String,
+) {
+    Text(
+        topLabel,
+        style = MaterialTheme.typography.labelLarge,
+        color = MaterialTheme.colorScheme.outline,
+        modifier = Modifier.padding(10.dp)
+    )
+
+    list.forEachIndexed { index, item ->
+        val shape = when {
+            list.size == 1 -> RoundedCornerShape(20.dp)
+            index == 0 -> RoundedCornerShape(
+                topStart = 20.dp,
+                topEnd = 20.dp,
+                bottomStart = 5.dp,
+                bottomEnd = 5.dp
+            )
+            index == list.size - 1 -> RoundedCornerShape(
+                bottomStart = 20.dp,
+                bottomEnd = 20.dp,
+                topStart = 5.dp,
+                topEnd = 5.dp
+            )
+            else -> RoundedCornerShape(5.dp)
+
+
+        }
+        when (item) {
+            is SettingItemData.ClickItem -> {
+                ClickItem(item.title,item.label,shape,item.onClick)
+            }
+            is SettingItemData.SwitchItem -> {
+                SettingItem(
+                    title = item.title,
+                    label = item.label,
+                    shape,
+                    { item.onChange(it) },
+                    item.value
+                )
+            }
+        }
+
+    }
+}
+
+@Composable
+fun SettingItem(
+    title: String,
+    label: String,
+    clip: RoundedCornerShape,
+    onChange: (Boolean) -> Unit,
+    checked: Boolean
+) {
+    Row(
+        modifier = Modifier
+            .padding(bottom = 3.dp)
+            .height(80.dp)
+            .clip(clip)
+            .fillMaxWidth()
+            .background(Color.White)
+            .padding(15.dp), verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+
+    ) {
+
+        Column(
+            modifier = Modifier
+                .fillMaxHeight(),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(text = title, style = MaterialTheme.typography.titleMedium)
+            Text(text = label, style = MaterialTheme.typography.labelMedium)
+        }
+        Switch(checked = checked, onCheckedChange = { onChange(it) }, thumbContent = {
+            Icon(
+                imageVector = Icons.Filled.Check,
+                contentDescription = null,
+                modifier = Modifier.size(SwitchDefaults.IconSize),
+            )
+        })
+    }
+}
+
+@Composable
+fun ClickItem(title: String,
+              label: String,
+              clip: RoundedCornerShape,
+              onClick: () -> Unit) {
+    Row(
+        modifier = Modifier
+            .padding(bottom = 3.dp)
+            .height(80.dp)
+            .clip(clip)
+            .clickable(onClick = onClick)
+            .fillMaxWidth()
+            .background(Color.White)
+            .padding(15.dp)
+            , verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+
+    ) {
+
+        Column(
+            modifier = Modifier
+                .fillMaxHeight(),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(text = title, style = MaterialTheme.typography.titleMedium)
+            Text(text = label, style = MaterialTheme.typography.labelMedium)
+        }
     }
 }
